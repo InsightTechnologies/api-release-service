2528e7cd216da863bf63db2c14ee0e779990ba13
MaheshMediboyina <MaheshMediboyina@users.noreply.github.com>
2019-05-18 01:36:15 +0530

    Updated Logic

--- src/main/java/com/miracle/release/exception/ReleaseErrorCode.java
+++ src/main/java/com/miracle/release/exception/ReleaseErrorCode.java
@@ -1,5 +1,11 @@
 package com.miracle.release.exception;
 
-public class ReleaseErrorCode {
-
+public interface ReleaseErrorCode {
+	String INVALID_FILTER_TYPE = "MSB.REL.IFT.000";
+	String UNABLE_TO_ORDER_FEATURE_LIST = "MSB.REL.UOF.000";
+	String UNABLE_TO_GET_STORIES_FROM_FEATURE = "MSB.REL.UGS.000";
+	String UNABLE_TO_ESTIMATE_EFFORT = "MSB.REL.UEF.000";
+	String UNABLE_TO_GET_FEATURES = "MSB.REL.UGF.000";
+	
+	String RELEASE_CONTROLLER_UNKNOWN_EXCEPTION = "MSB.REL.UN.000";
 }
